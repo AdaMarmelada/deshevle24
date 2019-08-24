f8e199bd7a8c95bbefe626667803bb2e7039ce24
Aleksey Kolesnichenko <alekseykolesnichenko@MacBook-Air-Aleksey.local>
2019-08-24 17:00:23 +0300

    add adaptive

--- markup/js/main.js
+++ markup/js/main.js
@@ -103,8 +103,13 @@ $(document).ready(function(){
     //toggle class
     $("body").on("click", ".js-toggle_class", function(e){
         e.preventDefault();
-        var $target = $($(this).attr("data-target")),
+        var $target,
             className = $(this).attr("data-class");
+        if ($(this).attr("data-is_parent") == "true") {
+            $target = $(this).parents($(this).attr("data-target")).first();
+        } else {
+            $target = $($(this).attr("data-target"));
+        }
         if ($target.hasClass(className)) {
             $target.removeClass(className);
             $(this).removeClass(className);
